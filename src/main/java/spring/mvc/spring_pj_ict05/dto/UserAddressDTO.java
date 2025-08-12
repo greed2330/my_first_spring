6a59c578bf40260c2f449b562d6fc085ad93ef90
greed <oscar2003hu@gmail.com>
2025-08-12 14:40:15 +0900

    회원가입-우편번호 서비스 기능 구현

--- src/main/java/spring/mvc/spring_pj_ict05/dto/UserAddressDTO.java
+++
@@ -1,9 +0,0 @@
-package spring.mvc.spring_pj_ict05.dto;
-
-public class UserAddressDTO {
-	private String postcode; //우편번호
-	private String roadAddress; //도로명
-	private String jibunAddress; //지번
-	private String detailAddress;//상세주소
-	
-}
